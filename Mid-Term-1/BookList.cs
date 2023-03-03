e14bc458223ed2a26533a0f157c5ecb6339c0b63
Manvibolreach Ouk <vibolreach.m.ouk@gmail.com>
2023-03-03 12:56:42 -0500

    feat:mid_term project

---
+++ Mid-Term-1/BookList.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mid_Term_1
+{
+    class BookList
+    {
+        private LinkedList<Book> books = new LinkedList<Book>();
+
+        public void AddBook(Book book)
+        {
+            if (!books.Any(b => b.ISBN == book.ISBN))
+            {
+                book.Id = books.Count + 1;
+                books.AddLast(book);
+                Console.WriteLine("Book added successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Book already exists in the list!");
+            }
+        }
+
+        public void RemoveBook(string ISBN)
+        {
+            bool removed = false;
+            foreach (var book in books)
+            {
+                if (book.ISBN == ISBN)
+                {
+                    books.Remove(book);
+                    removed = true;
+                    Console.WriteLine("Book removed successfully!");
+                    break;
+                }
+            }
+
+            if (!removed)
+            {
+                Console.WriteLine("Book not found in the list!");
+            }
+        }
+
+        public void FindBook(string title)
+        {
+            bool found = false;
+            foreach (var book in books)
+            {
+                if (book.Title == title)
+                {
+                    Console.WriteLine($"Book found: {book.Title} by {book.Author}, ISBN: {book.ISBN}, ID: {book.Id}");
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Book not found in the list!");
+            }
+        }
+
+        public void ListBooksSorted()
+        {
+            var bookList = books.ToList();
+            bookList.Sort();
+
+            Console.WriteLine("Books sorted by ISBN:");
+            foreach (var book in bookList)
+            {
+                Console.WriteLine($"ID: {book.Id}, Title: {book.Title}, Author: {book.Author}, ISBN: {book.ISBN}");
+            }
+        }
+
+        public void ListBooksUnsorted()
+        {
+            Console.WriteLine("All books in the list:");
+            foreach (var book in books)
+            {
+                Console.WriteLine($"ID: {book.Id}, Title: {book.Title}, Author: {book.Author}, ISBN: {book.ISBN}");
+            }
+        }
+    }
+
+    public class Book : IComparable<Book>
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            return ISBN.CompareTo(other.ISBN);
+        }
+    }
+}
---
+++ Mid-Term-1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Mid_Term_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookList bookList = new BookList();
+
+            while (true)
+            {
+                Console.WriteLine("+---------------------Welcome------------------------+");
+                Console.WriteLine("|         Enter 1 to add a book                      |");
+                Console.WriteLine("|         Enter 2 to remove a book                   |");
+                Console.WriteLine("|         Enter 3 to find a book                     |");
+                Console.WriteLine("|         Enter 4 to list all books sorted by ISBN   |");
+                Console.WriteLine("|         Enter 5 to list all books unsorted         |");
+                Console.WriteLine("|         Enter 0 to exit                            |");
+                Console.WriteLine("+----------------------------------------------------+");
+                Console.Write("Press the letter corresponding to your choice-> ");
+                string input = Console.ReadLine();
+                int choice;
+                if (int.TryParse(input, out choice))
+                {
+                    switch (choice)
+                    {
+                        case 0:
+                            Console.WriteLine("Exiting program...");
+                            return;
+                        case 1:
+                            // Add a book
+                            Console.WriteLine("Enter book title:");
+                            string title = Console.ReadLine();
+                            Console.WriteLine("Enter book author:");
+                            string author = Console.ReadLine();
+                            Console.WriteLine("Enter book ISBN:");
+                            string isbn = Console.ReadLine();
+
+                            Book book = new Book
+                            {
+                                Title = title,
+                                Author = author,
+                                ISBN = isbn
+                            };
+
+                            bookList.AddBook(book);
+                            break;
+                        case 2:
+                            // Remove a book
+                            Console.WriteLine("Enter book ISBN:");
+                            string isbnToRemove = Console.ReadLine();
+                            bookList.RemoveBook(isbnToRemove);
+                            break;
+                        case 3:
+                            // Find a book
+                            Console.WriteLine("Enter book title:");
+                            string titleToFind = Console.ReadLine();
+                            bookList.FindBook(titleToFind);
+                            break;
+                        case 4:
+                            // List all books sorted by ISBN
+                            bookList.ListBooksSorted();
+                            break;
+                        case 5:
+                            // List all books unsorted
+                            bookList.ListBooksUnsorted();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid input!");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
